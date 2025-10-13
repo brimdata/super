b707672e0f9b7570c791cac0030889287976d087
Matthew Nibecker <hello@mattnibecker.com>
2025-10-13 13:09:38 -0700

    optimizer: Convert cross joins to hash joins
    This commit adds functionality to the optimizer that rewrites cross
    joins with equality comparisons filters into hash joins where
    appropriate.

--- compiler/optimizer/join.go
+++ compiler/optimizer/join.go
@@ -27,6 +27,84 @@ func replaceJoinWithHashJoin(seq dag.Seq) {
 	})
 }
 
+func liftFilterConvertCrossJoin(seq dag.Seq) dag.Seq {
+	var filter *dag.FilterOp
+	var i int
+	for i = range len(seq) - 3 {
+		_, isfork := seq[i].(*dag.ForkOp)
+		_, _, _, isjoin := isJoin(seq[i+1])
+		var isfilter bool
+		filter, isfilter = seq[i+2].(*dag.FilterOp)
+		if isfork && isjoin && isfilter {
+			break
+		}
+	}
+	if filter == nil {
+		return seq
+	}
+	in := splitPredicate(filter.Expr)
+	var exprs []dag.Expr
+	for _, e := range in {
+		if b, ok := e.(*dag.BinaryExpr); ok && b.Op == "==" && convertCrossJoinToHashJoin(seq[i:], b.LHS, b.RHS) {
+			continue
+		}
+		exprs = append(exprs, e)
+	}
+	if len(exprs) == 0 {
+		seq.Delete(i+2, i+3)
+	} else if len(exprs) != len(in) {
+		seq[i+2] = dag.NewFilterOp(buildConjunction(exprs))
+	}
+	return seq
+}
+
+func convertCrossJoinToHashJoin(seq dag.Seq, lhs, rhs dag.Expr) bool {
+	fork, isfork := seq[0].(*dag.ForkOp)
+	leftAlias, rightAlias, style, isjoin := isJoin(seq[1])
+	if !isfork || !isjoin {
+		return false
+	}
+	if len(fork.Paths) != 2 {
+		panic(fork)
+	}
+	lhsFirst, lok := firstThisPathComponent(lhs)
+	rhsFirst, rok := firstThisPathComponent(rhs)
+	if !lok || !rok {
+		return false
+	}
+	lhs, rhs = dag.CopyExpr(lhs), dag.CopyExpr(rhs)
+	stripFirstThisPathComponent(lhs)
+	stripFirstThisPathComponent(rhs)
+	if lhsFirst == rhsFirst {
+		if lhsFirst == leftAlias {
+			return convertCrossJoinToHashJoin(fork.Paths[0], lhs, rhs)
+		}
+		if lhsFirst == rightAlias {
+			return convertCrossJoinToHashJoin(fork.Paths[1], lhs, rhs)
+		}
+		return false
+	}
+	if style != "cross" {
+		return false
+	}
+	if lhsFirst != leftAlias {
+		lhsFirst, rhsFirst = rhsFirst, lhsFirst
+		lhs, rhs = rhs, lhs
+	}
+	if lhsFirst != leftAlias || rhsFirst != rightAlias {
+		return false
+	}
+	seq[1] = &dag.HashJoinOp{
+		Kind:       "HashJoinOp",
+		Style:      "inner",
+		LeftAlias:  leftAlias,
+		RightAlias: rightAlias,
+		LeftKey:    lhs,
+		RightKey:   rhs,
+	}
+	return true
+}
+
 func equiJoinKeyExprs(e dag.Expr, leftAlias, rightAlias string) (left, right dag.Expr, ok bool) {
 	b, ok := e.(*dag.BinaryExpr)
 	if !ok || b.Op != "==" {
