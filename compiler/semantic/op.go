brimdata/super

commit 517f23d7c88e68ebc1c85c32771a073a2e90a358

switch to pragma-controlled Postgres style relational scoping (#6495)
This commit changes the relational scoping rules to follow Postgres
except for GROUP BY expressions which check column aliases before
the input table (like Google SQL).  This is favored as the default
for SuperSQL because it doesn't change binding when a dynamic input
transitions to a typed input (and thus a field that was present but
unknown in the input changes the GROUP BY binding of a column alias).

This behavior can be overridden now with "pragma pg=true" which says
to follow Postgres semantics and favor the input table over any column
alias in GROUP BY expressions.

As a result, column aliases are no longer visible in WHERE clauses
or SELECT expressions in line with Postgres semantics (and ANSI
and Google SQL).

We added some light documentation to the pragma section of the book
in this PR, but more details and an explanation of the rationale for
this design will be forthcoming in a future PR from the book-sql branch.

compiler/semantic/op.go
@@ -1251,29 +1251,24 @@ func (t *translator) opDecl(d *ast.OpDecl) {
 }
 
 func (t *translator) pragmaDecl(d *ast.PragmaDecl) {
-	switch d.Name.Name {
+	name := d.Name.Name
+	if _, ok := t.scope.pragmas[name]; ok {
+		t.error(d.Name, fmt.Errorf("%q redefined", name))
+		return
+	}
+	switch name {
 	case "index_base":
-		if _, ok := t.scope.pragmas["index_base"]; ok {
-			t.error(d.Name, errors.New("index_base defined multiple times in scope"))
-			return
-		}
-		e := t.expr(d.Expr)
-		val, ok := t.mustEval(e)
-		if !ok {
-			return
-		}
-		if !super.IsInteger(val.Type().ID()) || val.IsNull() {
-			t.error(d.Expr, fmt.Errorf("index_base value must be an integer: %s", sup.FormatValue(val)))
-			return
-		}
-		v := int(val.AsInt())
-		if v < 0 || v > 1 {
+		if v := t.mustEvalPositiveInteger(d.Expr); v <= 1 {
+			t.scope.pragmas["index_base"] = v
+		} else {
 			t.error(d.Name, errors.New("index_base must be 0 or 1"))
-			return
 		}
-		t.scope.pragmas["index_base"] = v
+	case "pg":
+		if v, ok := t.mustEvalBool(d.Expr); ok {
+			t.scope.pragmas["pg"] = v
+		}
 	default:
-		t.error(d.Name, fmt.Errorf("unknown pragma %q", d.Name.Name))
+		t.error(d.Name, fmt.Errorf("unknown pragma %q", name))
 	}
 }
 
@@ -1555,9 +1550,21 @@ func (t *translator) mustEvalString(e sem.Expr) (field string, ok bool) {
 	return "", false
 }
 
+func (t *translator) mustEvalBool(in ast.Expr) (bool, bool) {
+	val, ok := t.mustEval(t.expr(in))
+	if ok {
+		if super.TypeUnder(val.Type()) != super.TypeBool {
+			t.error(in, errors.New("expected type bool"))
+			return false, false
+		}
+		return val.AsBool(), true
+	}
+	return false, false
+}
+
 func (t *translator) maybeEvalString(e sem.Expr) (field string, ok bool) {
 	val, ok := t.maybeEval(e)
-	if ok && !val.IsError() && super.TypeUnder(val.Type()) == super.TypeString {
+	if ok && super.TypeUnder(val.Type()) == super.TypeString {
 		return string(val.Bytes()), true
 	}
 	return "", false
@@ -1570,12 +1577,12 @@ func (t *translator) mustEvalPositiveInteger(ae ast.Expr) int {
 		return 0
 	}
 	if !super.IsInteger(val.Type().ID()) || val.IsNull() {
-		t.error(ae, fmt.Errorf("expression value must be an integer value: %s", sup.FormatValue(val)))
+		t.error(ae, errors.New("expected integer"))
 		return 0
 	}
 	v := int(val.AsInt())
 	if v < 0 {
-		t.error(ae, errors.New("expression value must be a positive integer"))
+		t.error(ae, errors.New("expected positive integer"))
 		return 0
 	}
 	return v

compiler/semantic/schema.go
@@ -76,11 +76,9 @@ type (
 
 		// Columns holds the projection.  We bookkeep both the ast expression
 		// and sem tree expr so we can do lateral column alias macro substition.
-		// Also, a SELECT column can refer to only the lateral columns to its left
-		// so latStop limits the extent of the column lookup to avoid
-		// an infinite expansion cycle.
+		// The lateral field is true when lateral column resolution is allowed.
 		columns []column
-		latStop int
+		lateral bool
 
 		// The output schema is always static, e.g., a dynamic input is
 		// always wrapped in a column.  Also, the projected columns referenced

compiler/semantic/scope.go
@@ -68,6 +68,15 @@ func (s *Scope) lookupEntry(name string) *entry {
 	return nil
 }
 
+func (s *Scope) lookupPragma(name string) any {
+	for scope := s; scope != nil; scope = scope.parent {
+		if p, ok := scope.pragmas[name]; ok {
+			return p
+		}
+	}
+	return nil
+}
+
 func (s *Scope) lookupExpr(t *translator, name string) sem.Expr {
 	if entry := s.lookupEntry(name); entry != nil {
 		// function parameters hide exteral definitions as you don't
@@ -132,6 +141,12 @@ func (s *Scope) resolve(t *translator, n ast.Node, path field.Path) (sem.Expr, e
 		}
 		return nil, errors.New("cannot reference 'this' in relational context; consider the 'values' operator") //XXX new error?
 	}
+	var inputFirst bool
+	if pg := s.lookupPragma("pg"); pg != nil {
+		if b, ok := pg.(bool); ok {
+			inputFirst = b
+		}
+	}
 	// We give priority to lateral column aliases which diverges from
 	// Postgres semantics (which checks the input table first) but follows
 	// Google SQL semantics.  We favor this approach so we can avoid
@@ -141,8 +156,10 @@ func (s *Scope) resolve(t *translator, n ast.Node, path field.Path) (sem.Expr, e
 	// to a column aliases otherwise as data evolves (and such a column shows up)
 	// the binding of identifiers changes and query results can change.  By
 	// resolving lateral aliases first, we avoid this problem.
-	if e := resolveLateralColumn(t, sch, path[0]); e != nil {
-		return extend(n, e, path[1:]), nil
+	if sch, ok := sch.(*selectSchema); ok && sch.lateral && !inputFirst {
+		if e := resolveLateralColumn(t, sch, path[0]); e != nil {
+			return extend(n, e, path[1:]), nil
+		}
 	}
 	out, dyn, err := sch.resolveUnqualified(path[0])
 	if err != nil {
@@ -156,6 +173,11 @@ func (s *Scope) resolve(t *translator, n ast.Node, path field.Path) (sem.Expr, e
 			}
 		}
 		return sem.NewThis(n, append(out, path[1:]...)), nil
+	}
+	if sch, ok := sch.(*selectSchema); ok && sch.lateral && inputFirst {
+		if e := resolveLateralColumn(t, sch, path[0]); e != nil {
+			return extend(n, e, path[1:]), nil
+		}
 	}
 	if len(path) == 1 {
 		// Single identifier didn't resolve to column so let's see if there's
@@ -209,14 +231,13 @@ func resolveLateralColumn(t *translator, s schema, col string) sem.Expr {
 		// lateral columns available only inside select bodies
 		return nil
 	}
-	for k := range sch.latStop {
+	for k := range len(sch.columns) {
 		c := sch.columns[k]
 		if c.lateral && c.name == col {
-			save := sch.latStop
 			defer func() {
-				sch.latStop = save
+				sch.lateral = true
 			}()
-			sch.latStop = k
+			sch.lateral = false
 			return t.expr(c.astExpr)
 		}
 	}
@@ -224,13 +245,8 @@ func resolveLateralColumn(t *translator, s schema, col string) sem.Expr {
 }
 
 func (s *Scope) indexBase() int {
-	for {
-		if v, ok := s.pragmas["index_base"]; ok {
-			return v.(int)
-		}
-		if s.parent == nil {
-			return 0
-		}
-		s = s.parent
+	if v := s.lookupPragma("index_base"); v != nil {
+		return v.(int)
 	}
+	return 0
 }

compiler/semantic/sql.go
@@ -49,13 +49,14 @@ func (t *translator) sqlSelect(sel *ast.SQLSelect, demand []ast.Expr, seq sem.Se
 		t.scope.schema = save
 	}()
 	// form column slots (and expand *'s) so we can resolve lateral column aliases
+	sch.lateral = false
 	sch.columns = t.formProjection(sch, sel.Selection.Args)
-	sch.latStop = len(sch.columns)
 	seq = valuesExpr(wrapThis(sel, "in"), seq)
 	if sel.Where != nil {
 		where := t.expr(sel.Where)
 		seq = append(seq, sem.NewFilter(sel.Where, where))
 	}
+	sch.lateral = true
 	sch.groupings = t.groupBy(sch, sel.GroupBy)
 	sch.aggOk = true
 	// Make sure any aggs needed by ORDER BY and computed.
@@ -65,7 +66,9 @@ func (t *translator) sqlSelect(sel *ast.SQLSelect, demand []ast.Expr, seq sem.Se
 	// Next analyze all the columns so we know if there are aggregate functions
 	// (in particular, we know this when there is no GROUP BY) so after this,
 	// we know for sure if the output is grouped or not.
+	sch.lateral = false
 	t.projection(sch, sch.columns)
+	sch.lateral = true
 	// We now stitch together the fragments into pipeline operators depending
 	// on whether the ouput is grouped or not.
 	var having sem.Expr
@@ -166,12 +169,10 @@ func (t *translator) projection(sch *selectSchema, columns []column) {
 	for k := range columns {
 		// Translates all expressions that weren't already expanded
 		// from * patterns.
-		sch.latStop = k
 		if columns[k].semExpr == nil {
 			columns[k].semExpr = t.expr(columns[k].astExpr)
 		}
 	}
-	sch.latStop = len(sch.columns)
 }
 
 func (t *translator) emitProjection(columns []column, grouped bool, seq sem.Seq) (sem.Seq, *staticSchema) {

compiler/semantic/ztests/pragma-pg-type.yaml
@@ -1,0 +1,6 @@
+spq: pragma pg="foo" pass
+
+error: |
+  expected type bool at line 1, column 11:
+  pragma pg="foo" pass
+            ~~~~~

compiler/semantic/ztests/scope-order-by.yaml
@@ -22,3 +22,76 @@ output: |
   {a:2}
   {a:1}
   {a:0}
+
+---
+
+spq: |
+  pragma pg = true
+  select a as b
+  from (values (0,4),(1,3),(2,2)) T(a,b)
+  group by a,b
+  having b >= 1
+  order by b
+
+output: |
+  {b:0}
+  {b:1}
+  {b:2}
+
+---
+
+spq: |
+  pragma pg = true
+  select a
+  from (values (0,4),(1,3),(2,2)) T(a,b)
+  group by a,b
+  having b >= 1
+  order by T.b
+
+output: |
+  {a:2}
+  {a:1}
+  {a:0}
+
+---
+
+spq: |
+  pragma pg = true
+  select a
+  from (values (0,4),(1,3),(2,2)) T(a,b)
+  group by a,b
+  having b >= 1
+  order by T.b
+
+output: |
+  {a:2}
+  {a:1}
+  {a:0}
+
+---
+
+spq: |
+  pragma pg=true
+  select
+    ( select collect(a) from (
+        select a/2 as a
+        from (values (0),(1),(2)) T(a)
+        group by a
+        order by a )
+    ) as x,
+    ( pragma pg=false
+      select collect(a) from (
+        select a/2 as a
+        from (values (0),(1),(2)) T(a)
+        group by a
+        order by a )
+    ) as y,
+    ( select collect(a) from (
+        select a/2 as a
+        from (values (0),(1),(2)) T(a)
+        group by a
+        order by a )
+    ) as z
+
+output: |
+  {x:[0,0,1],y:[0,1],z:[0,0,1]}

compiler/ztests/head.yaml
@@ -18,10 +18,10 @@ outputs:
       | output main
   - name: stderr
     data: |
-      expression value must be an integer value: 1. at line 1, column 6:
+      expected integer at line 1, column 6:
       head 1.
            ~~
-      expression value must be an integer value: "1" at line 1, column 6:
+      expected integer at line 1, column 6:
       head "1"
            ~~~
       cannot reference 'x' in constant expression at line 1, column 6:

compiler/ztests/pragma.yaml
@@ -14,14 +14,14 @@ outputs:
       pragma doesnotexist = 0 values null
              ~~~~~~~~~~~~
       // ===
-      index_base defined multiple times in scope at line 1, column 30:
+      "index_base" redefined at line 1, column 30:
       pragma index_base = 0 pragma index_base = 1 values null
                                    ~~~~~~~~~~
       // ===
-      index_base value must be an integer: "foo" at line 1, column 21:
+      expected integer at line 1, column 21:
       pragma index_base = "foo" values null
                           ~~~~~
       // ===
-      index_base must be 0 or 1 at line 1, column 8:
+      expected positive integer at line 1, column 21:
       pragma index_base = -1 values null
-             ~~~~~~~~~~
+                          ~~

commit cc076a53ae7976c00492b5872133755ce71ded1b

fix relational scoping bug introduced in #6495
This commit fixes a bug where ORDER BY scope was improperly
handled because the logic favored the input table over the
column aliases but the materialized output table has priority
over the input table once the SELECT body scope is left.
This would typically be handled by first looking into the output
table but we can't do this because we do expression matching
on the sem-tree expressions and we can't mix and match
input expressions with the output expressions.  The fix is
to look in the columns first (producing input table expressions)
and matching those expressions to the grouped output table
columns with the expression matcher.

compiler/semantic/op.go
@@ -409,7 +409,6 @@ func (t *translator) sortExpr(sch schema, s ast.SortExpr, reverse bool) sem.Sort
 		} else {
 			e = t.expr(s.Expr)
 		}
-
 	} else {
 		e = t.expr(s.Expr)
 	}

compiler/semantic/schema.go
@@ -76,7 +76,8 @@ type (
 
 		// Columns holds the projection.  We bookkeep both the ast expression
 		// and sem tree expr so we can do lateral column alias macro substition.
-		// The lateral field is true when lateral column resolution is allowed.
+		// The lateral field is true when lateral column resolution is allowed
+		// during the analysis GROUP BY expressions.
 		columns []column
 		lateral bool
 
@@ -186,21 +187,6 @@ func (j *joinSchema) resolveQualified(table, col string) (field.Path, error) {
 }
 
 func (s *selectSchema) resolveQualified(table, col string) (field.Path, error) {
-	if s.out != nil {
-		// The output scope is set after the select scope is almost closed so that
-		// ORDER BY can utilize the projected columns, which have precedence
-		// higher than anything else, except when recursively descending
-		// into the argument expression of agg functions, in which case,
-		// s.out is temporarily cleared (in t.aggFunc()) and scalars then resolve
-		// to input and lateral columns.
-		path, err := s.out.resolveQualified(table, col)
-		if err != nil {
-			return nil, err
-		}
-		if path != nil {
-			return append([]string{"out"}, path...), nil
-		}
-	}
 	path, err := s.in.resolveQualified(table, col)
 	if path != nil {
 		return append([]string{"in"}, path...), nil
@@ -287,7 +273,7 @@ func (j *joinUsingSchema) resolveUnqualified(col string) (field.Path, bool, erro
 func (s *selectSchema) resolveUnqualified(col string) (field.Path, bool, error) {
 	// This just looks for column in the input table.  The resolve() function
 	// looks at lateral column aliases if this fails.
-	if s.out != nil {
+	if s.out != nil && !s.isGrouped() {
 		// The output scope is set after the select scope is almost closed so that
 		// ORDER BY can utilize the projected columns, which have precedence
 		// higher than anything else, except when recursively descending

compiler/semantic/scope.go
@@ -147,6 +147,21 @@ func (s *Scope) resolve(t *translator, n ast.Node, path field.Path) (sem.Expr, e
 			inputFirst = b
 		}
 	}
+	// If the output projection is in place, then we are outside the
+	// SELECT body (e.g., in ORDER BY), so we need to first check
+	// the select column names before the inputs.  This is handled by
+	// other SQLs by looking in the output table, but we can't do that
+	// since we must can't mix output columns and input columns
+	// in the grouping expression matcher.  So we rely upon this column
+	// lookup to give us the input bindings and then match everything to its
+	// grouped output.
+	if sch, ok := sch.(*selectSchema); ok && sch.out != nil && sch.isGrouped() {
+		for _, c := range sch.columns {
+			if c.name == path[0] {
+				return extend(n, c.semExpr, path[1:]), nil
+			}
+		}
+	}
 	// We give priority to lateral column aliases which diverges from
 	// Postgres semantics (which checks the input table first) but follows
 	// Google SQL semantics.  We favor this approach so we can avoid
@@ -155,7 +170,8 @@ func (s *Scope) resolve(t *translator, n ast.Node, path field.Path) (sem.Expr, e
 	// is present in the input shouldn't control the scoping decision compared
 	// to a column aliases otherwise as data evolves (and such a column shows up)
 	// the binding of identifiers changes and query results can change.  By
-	// resolving lateral aliases first, we avoid this problem.
+	// resolving lateral aliases first, we avoid this problem.  This can
+	// be overridden with "pragma pg" to favor Postgres precedence.
 	if sch, ok := sch.(*selectSchema); ok && sch.lateral && !inputFirst {
 		if e := resolveLateralColumn(t, sch, path[0]); e != nil {
 			return extend(n, e, path[1:]), nil
@@ -227,7 +243,7 @@ func extend(n ast.Node, e sem.Expr, rest []string) sem.Expr {
 
 func resolveLateralColumn(t *translator, s schema, col string) sem.Expr {
 	sch, ok := s.(*selectSchema)
-	if !ok {
+	if !ok || !sch.lateral {
 		// lateral columns available only inside select bodies
 		return nil
 	}

compiler/semantic/sql.go
@@ -48,8 +48,9 @@ func (t *translator) sqlSelect(sel *ast.SQLSelect, demand []ast.Expr, seq sem.Se
 	defer func() {
 		t.scope.schema = save
 	}()
-	// form column slots (and expand *'s) so we can resolve lateral column aliases
-	sch.lateral = false
+	// Form column slots (and expand *'s) but delay analysis of expressions.
+	// This will let us properly resolve lateral column aliases from any
+	// GROUP BY expressions that reference them.
 	sch.columns = t.formProjection(sch, sel.Selection.Args)
 	seq = valuesExpr(wrapThis(sel, "in"), seq)
 	if sel.Where != nil {

compiler/ztests/sql/order-by-pg-scope.yaml
@@ -1,0 +1,16 @@
+script: |
+  super compile -C -O -I query.spq | grep sort
+
+inputs:
+  - name: query.spq
+    data: |
+      pragma pg=false
+      select a/2 as a
+      from (values (0),(1),(2)) T(a)
+      group by a
+      order by a
+
+outputs:
+  - name: stdout
+    data: |
+      | sort g.k0 asc nulls last