567cf347da4a53af0a0370d0d425f92df2d008ef
Matthew Nibecker <hello@mattnibecker.com>
2026-02-10 14:23:43 -0800

    vam.rename: update type of nested records (#6623)
    This commit fixes an issue in the vector runtime version of the rename
    operator where nested records were not getting assigned updated types.

--- runtime/vam/expr/renamer.go
+++ runtime/vam/expr/renamer.go
@@ -1,6 +1,8 @@
 package expr
 
 import (
+	"slices"
+
 	"github.com/brimdata/super"
 	"github.com/brimdata/super/runtime/sam/expr"
 	"github.com/brimdata/super/vector"
@@ -31,5 +33,15 @@ func (r *Renamer) eval(vecs ...vector.Any) vector.Any {
 	if err != nil {
 		return vector.NewWrappedError(r.sctx, err.Error(), vec)
 	}
-	return vector.NewRecord(val.Type().(*super.TypeRecord), recVec.Fields, recVec.Len(), recVec.Nulls)
+	return changeRecordType(recVec, val.Type().(*super.TypeRecord))
+}
+
+func changeRecordType(vec *vector.Record, typ *super.TypeRecord) *vector.Record {
+	fields := slices.Clone(vec.Fields)
+	for i, f := range typ.Fields {
+		if rtyp, ok := f.Type.(*super.TypeRecord); ok {
+			fields[i] = changeRecordType(vec.Fields[i].(*vector.Record), rtyp)
+		}
+	}
+	return vector.NewRecord(typ, fields, vec.Len(), vec.Nulls)
 }
