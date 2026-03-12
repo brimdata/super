iloveitaly/zed

Add support for db vacate command (#6706)

api/client/connection.go
@@ -19,6 +19,7 @@ import (
 	"github.com/brimdata/super/compiler/srcfiles"
 	"github.com/brimdata/super/db"
 	"github.com/brimdata/super/db/branches"
+	"github.com/brimdata/super/pkg/nano"
 	"github.com/brimdata/super/runtime/exec"
 	"github.com/brimdata/super/sio/bsupio"
 	"github.com/brimdata/super/sup"
@@ -371,6 +372,20 @@ func (c *Connection) delete(ctx context.Context, poolID ksuid.KSUID, branchName 
 	var commit api.CommitResponse
 	err := c.doAndUnmarshal(req, &commit)
 	return commit, err
+}
+
+func (c *Connection) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) (api.VacateResponse, error) {
+	path := urlPath("pool", pool, "vacate")
+	vals := make(url.Values)
+	vals.Add("ts", ts.Time().Format(time.RFC3339Nano))
+	if dryrun {
+		vals.Add("dryrun", "true")
+	}
+	path += "?" + vals.Encode()
+	req := c.NewRequest(ctx, http.MethodPost, path, nil)
+	var res api.VacateResponse
+	err := c.doAndUnmarshal(req, &res)
+	return res, err
 }
 
 func (c *Connection) Vacuum(ctx context.Context, pool, revision string, dryrun bool) (api.VacuumResponse, error) {

cmd/super/db/vacate/command.go
@@ -1,17 +1,25 @@
 package vacate
 
 import (
+	"context"
 	"errors"
 	"flag"
+	"fmt"
+	"strings"
 
+	"github.com/brimdata/super/cli/poolflags"
 	"github.com/brimdata/super/cmd/super/db"
+	"github.com/brimdata/super/db/api"
+	"github.com/brimdata/super/dbid"
 	"github.com/brimdata/super/pkg/charm"
+	"github.com/brimdata/super/pkg/nano"
+	"github.com/brimdata/super/pkg/plural"
 )
 
 var spec = &charm.Spec{
 	Name:  "vacate",
-	Usage: "vacate [options] commit",
-	Short: "compact a pool's commit history by squashing old commit objects",
+	Usage: "vacate [options] [timestamp]",
+	Short: "compact a pool's commit history by removing old commit objects",
 	Long: `
 See https://superdb.org/command/db.html#super-db-vacate
 `,
@@ -24,12 +32,79 @@ func init() {
 
 type Command struct {
 	*db.Command
+	poolFlags poolflags.Flags
+	dryrun    bool
+	force     bool
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
-	return &Command{Command: parent.(*db.Command)}, nil
+	c := &Command{Command: parent.(*db.Command)}
+	c.poolFlags.SetFlags(f)
+	f.BoolVar(&c.dryrun, "dryrun", false, "view the number of commits to be deleted")
+	f.BoolVar(&c.force, "f", false, "do not prompt for confirmation")
+	return c, nil
 }
 
 func (c *Command) Run(args []string) error {
-	return errors.New("issue #2545")
+	ctx, cleanup, err := c.Init()
+	if err != nil {
+		return err
+	}
+	defer cleanup()
+	db, err := c.DBFlags.Open(ctx)
+	if err != nil {
+		return err
+	}
+	at, err := c.poolFlags.HEAD()
+	if err != nil {
+		return err
+	}
+	var ts nano.Ts
+	if len(args) > 0 {
+		ts, err = nano.ParseRFC3339Nano([]byte(args[0]))
+	} else {
+		ts, err = c.getTsFromCommitish(ctx, db, at)
+	}
+	if err != nil {
+		return err
+	}
+	verb := "would vacate"
+	if !c.dryrun {
+		verb = "vacated"
+		if err := c.confirm(ctx, ts); err != nil {
+			return err
+		}
+	}
+	cids, err := db.Vacate(ctx, at.Pool, ts, c.dryrun)
+	if err != nil {
+		return err
+	}
+	if !c.DBFlags.Quiet {
+		fmt.Printf("%s %d commit%s\n", verb, len(cids), plural.Slice(cids, "s"))
+	}
+	return nil
+}
+
+func (c *Command) getTsFromCommitish(ctx context.Context, db api.Interface, at *dbid.Commitish) (nano.Ts, error) {
+	commit, err := api.GetCommit(ctx, db, at.Pool, at.Branch)
+	if err != nil {
+		return 0, err
+	}
+	return commit.Date, nil
+}
+
+func (c *Command) confirm(ctx context.Context, ts nano.Ts) error {
+	if c.force {
+		return nil
+	}
+	fmt.Printf("Are you sure you want to vacate history order than %s? There is no going back... [y|n]\n", ts)
+	var input string
+	if _, err := fmt.Scanln(&input); err != nil {
+		return err
+	}
+	input = strings.ToLower(input)
+	if input == "y" || input == "yes" {
+		return nil
+	}
+	return errors.New("operation canceled")
 }

db/api/local.go
@@ -12,6 +12,7 @@ import (
 	"github.com/brimdata/super/db"
 	"github.com/brimdata/super/dbid"
 	"github.com/brimdata/super/order"
+	"github.com/brimdata/super/pkg/nano"
 	"github.com/brimdata/super/pkg/storage"
 	"github.com/brimdata/super/runtime"
 	"github.com/brimdata/super/runtime/exec"
@@ -204,6 +205,18 @@ func (l *local) DeleteVectors(ctx context.Context, pool, revision string, ids []
 		return ksuid.Nil, err
 	}
 	return branch.DeleteVectors(ctx, ids, message.Author, message.Body)
+}
+
+func (l *local) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	poolID, err := l.PoolID(ctx, pool)
+	if err != nil {
+		return nil, err
+	}
+	p, err := l.db.OpenPool(ctx, poolID)
+	if err != nil {
+		return nil, err
+	}
+	return p.Vacate(ctx, ts, dryrun)
 }
 
 func (l *local) Vacuum(ctx context.Context, pool, revision string, dryrun bool) ([]ksuid.KSUID, error) {

db/api/remote.go
@@ -14,6 +14,7 @@ import (
 	"github.com/brimdata/super/dbid"
 	"github.com/brimdata/super/order"
 	"github.com/brimdata/super/pkg/field"
+	"github.com/brimdata/super/pkg/nano"
 	"github.com/brimdata/super/sbuf"
 	"github.com/brimdata/super/sio"
 	"github.com/brimdata/super/sio/bsupio"
@@ -146,6 +147,11 @@ func (r *remote) AddVectors(ctx context.Context, pool, revision string, objects 
 func (r *remote) DeleteVectors(ctx context.Context, pool, revision string, ids []ksuid.KSUID, message api.CommitMessage) (ksuid.KSUID, error) {
 	res, err := r.conn.DeleteVectors(ctx, pool, revision, ids, message)
 	return res.Commit, err
+}
+
+func (r *remote) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	res, err := r.conn.Vacate(ctx, pool, ts, dryrun)
+	return res.CommitIDs, err
 }
 
 func (r *remote) Vacuum(ctx context.Context, pool, revision string, dryrun bool) ([]ksuid.KSUID, error) {

db/pool.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/fs"
 	"runtime"
+	"slices"
+	"strings"
 	"sync"
 
 	"github.com/brimdata/super"
@@ -14,6 +16,7 @@ import (
 	"github.com/brimdata/super/db/data"
 	"github.com/brimdata/super/db/pools"
 	"github.com/brimdata/super/dbid"
+	"github.com/brimdata/super/pkg/nano"
 	"github.com/brimdata/super/pkg/storage"
 	"github.com/brimdata/super/runtime/sam/expr"
 	"github.com/brimdata/super/sio"
@@ -209,6 +212,74 @@ func (p *Pool) BatchifyBranchTips(ctx context.Context, sctx *super.Context, f ex
 // XXX this is inefficient but is only meant for interactive queries...?
 func (p *Pool) ObjectExists(ctx context.Context, id ksuid.KSUID) (bool, error) {
 	return p.engine.Exists(ctx, data.SequenceURI(p.DataPath, id))
+}
+
+func (p *Pool) Vacate(ctx context.Context, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	if !dryrun {
+		if err := p.vacateBranchStore(ctx, ts, dryrun); err != nil {
+			return nil, err
+		}
+	}
+	return p.vacateCommits(ctx, ts, dryrun)
+}
+
+func (p *Pool) vacateCommits(ctx context.Context, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	main, err := p.Main(ctx)
+	if err != nil {
+		return nil, err
+	}
+	commit, err := p.commits.FindNearestToTs(ctx, main.Branch.Commit, ts)
+	if err != nil {
+		return nil, err
+	}
+	branches, err := p.branches.All(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var conflicts []string
+	for _, b := range branches {
+		// Find any branches whose history does not include commit and if any
+		// are found fail.
+		path, err := p.commits.Path(ctx, b.Commit)
+		if err != nil {
+			return nil, err
+		}
+		if !slices.Contains(path, commit) {
+			conflicts = append(conflicts, b.Name)
+		}
+	}
+	if len(conflicts) > 0 {
+		slices.Sort(conflicts)
+		v := "branch does not include commit nearest timestamp"
+		if len(conflicts) > 1 {
+			v = "branches do not include commit nearest timestamp"
+		}
+		return nil, fmt.Errorf("cannot vacate at selected time: %s in the deletion path: %s", v, strings.Join(conflicts, ", "))
+	}
+	if dryrun {
+		path, err := p.commits.Path(ctx, commit)
+		if err != nil {
+			return nil, err
+		}
+		return path[1:], nil
+	}
+	return p.commits.SetBase(ctx, commit)
+}
+
+func (p *Pool) vacateBranchStore(ctx context.Context, ts nano.Ts, dryrun bool) error {
+	var werr error
+	at, err := p.branches.EntryWhere(ctx, func(config *branches.Config) bool {
+		if config.Commit.IsNil() {
+			return false
+		}
+		var c *commits.Commit
+		_, c, werr = p.commits.GetBytes(ctx, config.Commit)
+		return werr != nil || ts >= c.Date
+	})
+	if err = errors.Join(err, werr); err != nil {
+		return err
+	}
+	return p.branches.TruncateHistory(ctx, at)
 }
 
 func (p *Pool) Vacuum(ctx context.Context, commit ksuid.KSUID, dryrun bool) ([]ksuid.KSUID, error) {

brimdata/super

api/client/connection.go
@@ -20,6 +20,7 @@ import (
 	"github.com/brimdata/zed/lake"
 	"github.com/brimdata/zed/lake/branches"
 	"github.com/brimdata/zed/lakeparse"
+	"github.com/brimdata/zed/pkg/nano"
 	"github.com/brimdata/zed/runtime/exec"
 	"github.com/brimdata/zed/zio/zngio"
 	"github.com/brimdata/zed/zson"
@@ -375,6 +376,20 @@ func (c *Connection) delete(ctx context.Context, poolID ksuid.KSUID, branchName 
 	var commit api.CommitResponse
 	err := c.doAndUnmarshal(req, &commit)
 	return commit, err
+}
+
+func (c *Connection) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) (api.VacateResponse, error) {
+	path := urlPath("pool", pool, "vacate")
+	vals := make(url.Values)
+	vals.Add("ts", ts.Time().Format(time.RFC3339Nano))
+	if dryrun {
+		vals.Add("dryrun", "true")
+	}
+	path += "?" + vals.Encode()
+	req := c.NewRequest(ctx, http.MethodPost, path, nil)
+	var res api.VacateResponse
+	err := c.doAndUnmarshal(req, &res)
+	return res, err
 }
 
 func (c *Connection) Vacuum(ctx context.Context, pool, revision string, dryrun bool) (api.VacuumResponse, error) {

cmd/zed/vacate/command.go
@@ -1,42 +1,106 @@
 package vacate
 
 import (
+	"context"
 	"errors"
 	"flag"
+	"fmt"
+	"strings"
 
+	"github.com/brimdata/zed/cli/poolflags"
 	"github.com/brimdata/zed/cmd/zed/root"
+	"github.com/brimdata/zed/lake/api"
+	"github.com/brimdata/zed/lakeparse"
 	"github.com/brimdata/zed/pkg/charm"
+	"github.com/brimdata/zed/pkg/nano"
+	"github.com/brimdata/zed/pkg/plural"
 )
 
 var Cmd = &charm.Spec{
 	Name:  "vacate",
-	Usage: "vacate [options] commit",
-	Short: "compact a pool's commit history by squashing old commit objects",
+	Usage: "vacate [options] [timestamp]",
+	Short: "compact a pool's commit history by removing old commit objects",
 	Long: `
-The vacate command compacts the commit history by squashing all of the commit
-objects in the history up to the indicated commit and removing the old commits.
-No other commit objects in the pool may point at any of the squashed commits.
-In particular, no branch may point to any commit that would be deleted.
-
-The branch history may contain pointers to old commit objects, but any attempt
-to access them will fail as the underlying commit history will be no longer available.
-
-DANGER ZONE.
-There is no prompting or second chances here so use carefully.
-Once the pool's commit history has been squashed and old commits is deleted,
-they cannot be recovered.
+See https://superdb.org/command/db.html#super-db-vacate
 `,
 	New: New,
 }
 
 type Command struct {
 	*root.Command
+	poolFlags poolflags.Flags
+	dryrun    bool
+	force     bool
 }
 
 func New(parent charm.Command, f *flag.FlagSet) (charm.Command, error) {
-	return &Command{Command: parent.(*root.Command)}, nil
+	c := &Command{Command: parent.(*root.Command)}
+	c.poolFlags.SetFlags(f)
+	f.BoolVar(&c.dryrun, "dryrun", false, "view the number of commits to be deleted")
+	f.BoolVar(&c.force, "f", false, "do not prompt for confirmation")
+	return c, nil
 }
 
 func (c *Command) Run(args []string) error {
-	return errors.New("issue #2545")
+	ctx, cleanup, err := c.Init()
+	if err != nil {
+		return err
+	}
+	defer cleanup()
+	db, err := c.LakeFlags.Open(ctx)
+	if err != nil {
+		return err
+	}
+	at, err := c.poolFlags.HEAD()
+	if err != nil {
+		return err
+	}
+	var ts nano.Ts
+	if len(args) > 0 {
+		ts, err = nano.ParseRFC3339Nano([]byte(args[0]))
+	} else {
+		ts, err = c.getTsFromCommitish(ctx, db, at)
+	}
+	if err != nil {
+		return err
+	}
+	verb := "would vacate"
+	if !c.dryrun {
+		verb = "vacated"
+		if err := c.confirm(ctx, ts); err != nil {
+			return err
+		}
+	}
+	cids, err := db.Vacate(ctx, at.Pool, ts, c.dryrun)
+	if err != nil {
+		return err
+	}
+	if !c.LakeFlags.Quiet {
+		fmt.Printf("%s %d commit%s\n", verb, len(cids), plural.Slice(cids, "s"))
+	}
+	return nil
+}
+
+func (c *Command) getTsFromCommitish(ctx context.Context, db api.Interface, at *lakeparse.Commitish) (nano.Ts, error) {
+	commit, err := api.GetCommit(ctx, db, at.Pool, at.Branch)
+	if err != nil {
+		return 0, err
+	}
+	return commit.Date, nil
+}
+
+func (c *Command) confirm(ctx context.Context, ts nano.Ts) error {
+	if c.force {
+		return nil
+	}
+	fmt.Printf("Are you sure you want to vacate history order than %s? There is no going back... [y|n]\n", ts)
+	var input string
+	if _, err := fmt.Scanln(&input); err != nil {
+		return err
+	}
+	input = strings.ToLower(input)
+	if input == "y" || input == "yes" {
+		return nil
+	}
+	return errors.New("operation canceled")
 }

lake/api/local.go
@@ -11,6 +11,7 @@ import (
 	"github.com/brimdata/zed/lake"
 	"github.com/brimdata/zed/lakeparse"
 	"github.com/brimdata/zed/order"
+	"github.com/brimdata/zed/pkg/nano"
 	"github.com/brimdata/zed/pkg/storage"
 	"github.com/brimdata/zed/runtime"
 	"github.com/brimdata/zed/runtime/exec"
@@ -207,6 +208,18 @@ func (l *local) DeleteVectors(ctx context.Context, pool, revision string, ids []
 		return ksuid.Nil, err
 	}
 	return branch.DeleteVectors(ctx, ids, message.Author, message.Body)
+}
+
+func (l *local) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	poolID, err := l.PoolID(ctx, pool)
+	if err != nil {
+		return nil, err
+	}
+	p, err := l.root.OpenPool(ctx, poolID)
+	if err != nil {
+		return nil, err
+	}
+	return p.Vacate(ctx, ts, dryrun)
 }
 
 func (l *local) Vacuum(ctx context.Context, pool, revision string, dryrun bool) ([]ksuid.KSUID, error) {

lake/api/remote.go
@@ -13,6 +13,7 @@ import (
 	"github.com/brimdata/zed/lakeparse"
 	"github.com/brimdata/zed/order"
 	"github.com/brimdata/zed/pkg/field"
+	"github.com/brimdata/zed/pkg/nano"
 	"github.com/brimdata/zed/zbuf"
 	"github.com/brimdata/zed/zio"
 	"github.com/brimdata/zed/zio/zngio"
@@ -145,6 +146,11 @@ func (r *remote) AddVectors(ctx context.Context, pool, revision string, objects 
 func (r *remote) DeleteVectors(ctx context.Context, pool, revision string, ids []ksuid.KSUID, message api.CommitMessage) (ksuid.KSUID, error) {
 	res, err := r.conn.DeleteVectors(ctx, pool, revision, ids, message)
 	return res.Commit, err
+}
+
+func (r *remote) Vacate(ctx context.Context, pool string, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	res, err := r.conn.Vacate(ctx, pool, ts, dryrun)
+	return res.CommitIDs, err
 }
 
 func (r *remote) Vacuum(ctx context.Context, pool, revision string, dryrun bool) ([]ksuid.KSUID, error) {

lake/pool.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io/fs"
 	"runtime"
+	"slices"
+	"strings"
 	"sync"
 
 	"github.com/brimdata/zed"
@@ -14,6 +16,7 @@ import (
 	"github.com/brimdata/zed/lake/data"
 	"github.com/brimdata/zed/lake/pools"
 	"github.com/brimdata/zed/lakeparse"
+	"github.com/brimdata/zed/pkg/nano"
 	"github.com/brimdata/zed/pkg/storage"
 	"github.com/brimdata/zed/runtime/sam/expr"
 	"github.com/brimdata/zed/zio"
@@ -212,6 +215,74 @@ func (p *Pool) BatchifyBranchTips(ctx context.Context, zctx *zed.Context, f expr
 // XXX this is inefficient but is only meant for interactive queries...?
 func (p *Pool) ObjectExists(ctx context.Context, id ksuid.KSUID) (bool, error) {
 	return p.engine.Exists(ctx, data.SequenceURI(p.DataPath, id))
+}
+
+func (p *Pool) Vacate(ctx context.Context, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	if !dryrun {
+		if err := p.vacateBranchStore(ctx, ts, dryrun); err != nil {
+			return nil, err
+		}
+	}
+	return p.vacateCommits(ctx, ts, dryrun)
+}
+
+func (p *Pool) vacateCommits(ctx context.Context, ts nano.Ts, dryrun bool) ([]ksuid.KSUID, error) {
+	main, err := p.Main(ctx)
+	if err != nil {
+		return nil, err
+	}
+	commit, err := p.commits.FindNearestToTs(ctx, main.Branch.Commit, ts)
+	if err != nil {
+		return nil, err
+	}
+	branches, err := p.branches.All(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var conflicts []string
+	for _, b := range branches {
+		// Find any branches whose history does not include commit and if any
+		// are found fail.
+		path, err := p.commits.Path(ctx, b.Commit)
+		if err != nil {
+			return nil, err
+		}
+		if !slices.Contains(path, commit) {
+			conflicts = append(conflicts, b.Name)
+		}
+	}
+	if len(conflicts) > 0 {
+		slices.Sort(conflicts)
+		v := "branch does not include commit nearest timestamp"
+		if len(conflicts) > 1 {
+			v = "branches do not include commit nearest timestamp"
+		}
+		return nil, fmt.Errorf("cannot vacate at selected time: %s in the deletion path: %s", v, strings.Join(conflicts, ", "))
+	}
+	if dryrun {
+		path, err := p.commits.Path(ctx, commit)
+		if err != nil {
+			return nil, err
+		}
+		return path[1:], nil
+	}
+	return p.commits.SetBase(ctx, commit)
+}
+
+func (p *Pool) vacateBranchStore(ctx context.Context, ts nano.Ts, dryrun bool) error {
+	var werr error
+	at, err := p.branches.EntryWhere(ctx, func(config *branches.Config) bool {
+		if config.Commit.IsNil() {
+			return false
+		}
+		var c *commits.Commit
+		_, c, werr = p.commits.GetBytes(ctx, config.Commit)
+		return werr != nil || ts >= c.Date
+	})
+	if err = errors.Join(err, werr); err != nil {
+		return err
+	}
+	return p.branches.TruncateHistory(ctx, at)
 }
 
 func (p *Pool) Vacuum(ctx context.Context, commit ksuid.KSUID, dryrun bool) ([]ksuid.KSUID, error) {