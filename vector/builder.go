edadf0bba3002b826a3da886a2b29aa766f3a771
Steven McCanne <steve@mccanne.com>
2026-03-19 07:47:49 -0700

    change fusion value subtypes in CSUP from type values to type IDs
    This commit improves the encoding of fusion values in CSUP and
    in the runtime by changing the representation from flattened
    type values to type IDs.  In the runtime, the subtypes are stored
    as type pointers.
    
    Also, the types are loaded on demand only when needed so if the
    vector runtime can compute a query without needing any subtype
    values, then the types are never loaded.
    
    With this change, the vector runtime can call the Subtypes() method
    on a vector.Fusion to get the types needed to perform a downcast
    and convert a Fusion to a Dynamic,
    
    This greatly improves the efficiencies and storing and loading
    fused values especially for large types in which a large subtype
    is stored at multiple levels in a value hiearchy as those subtypes
    are embedded in one another.
    
    To implement this, a local mapping of type IDs to type values is stored
    in the CSUP metadata header and mapped onto the query sctx in vcache.
    The entries in this table are full type values and a further improvement
    can come from encoding this table as fully-intered types as is done
    by the BSUP serialization of the type context (a future PR will DRY
    these two approaches).
    
    The BSUP representation of fusion subtypes is still a type value.

--- vector/builder.go
+++ vector/builder.go
@@ -263,24 +263,31 @@ func (u *unionBuilder) Build(sctx *super.Context) Any {
 }
 
 type fusionBuilder struct {
-	typ    *super.TypeFusion
-	values Builder
-	//XXX this will change
-	subTypes Builder
+	typ      *super.TypeFusion
+	values   Builder
+	subTypes []scode.Bytes
 }
 
 func newFusionBuilder(typ *super.TypeFusion) Builder {
-	return &fusionBuilder{typ: typ, values: NewBuilder(typ.Type), subTypes: newBytesStringTypeBuilder(super.TypeType)}
+	return &fusionBuilder{typ: typ, values: NewBuilder(typ.Type)}
 }
 
 func (f *fusionBuilder) Write(bytes scode.Bytes) {
 	it := bytes.Iter()
 	f.values.Write(it.Next())
-	f.subTypes.Write(it.Next())
+	f.subTypes = append(f.subTypes, it.Next())
 }
 
 func (f *fusionBuilder) Build(sctx *super.Context) Any {
-	return NewFusion(f.typ, f.values.Build(sctx), f.subTypes.Build(sctx).(*TypeValue))
+	types := make([]super.Type, 0, len(f.subTypes))
+	for _, tv := range f.subTypes {
+		t, err := sctx.LookupByValue(tv)
+		if err != nil {
+			panic(err)
+		}
+		types = append(types, t)
+	}
+	return NewFusion(sctx, f.typ, f.values.Build(sctx), types)
 }
 
 type enumBuilder struct {
