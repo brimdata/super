abd477468ae9775d494edd6622f11c3f6059b8ac
Steven McCanne <steve@mccanne.com>
2026-04-07 07:23:33 -0700

    catch boomerang isValid panics
    Previously, there was a problem with panics that occured during the
    boomerang test building phase where isValid reads input data outside
    of a go test name tied to the file being read.  This caused cryptic
    backtraces with little hint as to what input caused the panic to occur.
    
    This commit introduces logic to catch the panic and report a fatal
    testing error with the test name included along with the stack trace.

--- spq_test.go
+++ spq_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"testing"
@@ -34,7 +35,7 @@ func TestSPQ(t *testing.T) {
 
 	t.Run("boomerang", func(t *testing.T) {
 		t.Parallel()
-		data, err := loadZTestInputsAndOutputs(dirs)
+		data, err := loadZTestInputsAndOutputs(t, dirs)
 		require.NoError(t, err)
 		runAllBoomerangs(t, "arrows", data)
 		runAllBoomerangs(t, "csup", data)
@@ -67,7 +68,7 @@ func findZTests() (map[string]struct{}, error) {
 	return dirs, err
 }
 
-func loadZTestInputsAndOutputs(ztestDirs map[string]struct{}) (map[string]string, error) {
+func loadZTestInputsAndOutputs(t *testing.T, ztestDirs map[string]struct{}) (map[string]string, error) {
 	out := map[string]string{}
 	for dir := range ztestDirs {
 		bundles, err := ztest.Load(dir)
@@ -79,19 +80,19 @@ func loadZTestInputsAndOutputs(ztestDirs map[string]struct{}) (map[string]string
 				continue
 			}
 			testName := b.FileName + "/" + strconv.Itoa(b.Test.Line)
-			if i := b.Test.Input; i != nil && isValid(*i) {
+			if i := b.Test.Input; i != nil && isValid(t, testName, *i) {
 				out[testName+"/input"] = *i
 			}
-			if o := b.Test.Output; isValid(o) {
+			if o := b.Test.Output; isValid(t, testName, o) {
 				out[testName+"/output"] = o
 			}
 			for _, i := range b.Test.Inputs {
-				if i.Data != nil && isValid(*i.Data) {
+				if i.Data != nil && isValid(t, testName, *i.Data) {
 					out[testName+"/inputs/"+i.Name] = *i.Data
 				}
 			}
 			for _, o := range b.Test.Outputs {
-				if o.Data != nil && isValid(*o.Data) {
+				if o.Data != nil && isValid(t, testName, *o.Data) {
 					out[testName+"/outputs/"+o.Name] = *o.Data
 				}
 			}
@@ -102,7 +103,16 @@ func loadZTestInputsAndOutputs(ztestDirs map[string]struct{}) (map[string]string
 
 // isValid returns true if and only if s can be read fully without error by
 // anyio and contains at least one value.
-func isValid(s string) bool {
+func isValid(t *testing.T, name, s string) bool {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fatalf("boomerang panic reading test input: %s: %+v\n%s\n", name, r, debug.Stack())
+		}
+	}()
+	return isValidBare(s)
+}
+
+func isValidBare(s string) bool {
 	zrc, err := anyio.NewReader(super.NewContext(), strings.NewReader(s), anyio.ReaderOpts{})
 	if err != nil {
 		return false
