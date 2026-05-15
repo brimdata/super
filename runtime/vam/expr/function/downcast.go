a55f849bcab4029206f13d57b7000112197de2c1
Matthew Nibecker <hello@mattnibecker.com>
2026-05-15 14:00:04 -0700

    wip

--- runtime/vam/expr/function/downcast.go
+++ runtime/vam/expr/function/downcast.go
@@ -87,7 +87,7 @@ func (d *downcast) call(from vector.Any, types []super.Type) vector.Any {
 func (d *downcast) downcast(vec vector.Any, to super.Type) vector.Any {
 	// XXX Handle vec type All.
 	if _, ok := to.(*super.TypeUnion); !ok {
-		if vec.Kind() == vector.KindFusion {
+		if _, ok := vec.Type().(*super.TypeFusion); ok {
 			return d.downcastFusion(vec, to)
 		}
 	}
@@ -404,7 +404,8 @@ func (d *downcast) toUnion(vec vector.Any, to *super.TypeUnion) vector.Any {
 			return d.errSubtype(vec, to)
 		}
 		vec = d.downcast(vec, to.Types[tag])
-		return vector.NewUnion(to, make([]uint32, vec.Len()), []vector.Any{vec})
+		vecs := expr.AppendMissingToUnion(d.sctx, to, []vector.Any{vec})
+		return vector.NewUnion(to, make([]uint32, vec.Len()), vecs)
 	})
 }
 
